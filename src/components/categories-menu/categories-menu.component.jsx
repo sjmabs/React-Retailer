5429e1c6ec2a3b04f28073efca769d6bacc863bd
Shaun <shaun.mabin@hotmail.co.uk>
2023-06-16 17:29:27 +0100

    Updating our categories menu with a categories-menu component and a category-item component

---
+++ src/components/categories-menu/categories-menu.component.jsx
@@ -0,0 +1,15 @@
+import './categories-menu.styles.scss'
+import CategoryItem from '../category-item/category-item.component';
+
+const CategoriesMenu = ({categories}) => {
+
+    return (
+      <div className="categories-container">
+        {categories.map((category) => (
+          <CategoryItem key={category.id} category={category} />
+        ))}
+      </div>
+    );
+}
+
+export default CategoriesMenu 
