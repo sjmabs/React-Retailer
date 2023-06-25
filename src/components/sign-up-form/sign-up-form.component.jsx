2decf7138c8f2e3dbb6305497d1990cec53f7b0c
Shaun <shaun.mabin@hotmail.co.uk>
2023-06-25 11:29:00 +0100

    added form-input styles and generalized the form input component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 
+import FormInput from '../form-input/form-input.component';
+
 
 const defaultFormFields = {
     displayName: '',
@@ -56,12 +58,11 @@ const SignUpForm = () => {
     }
 
     return (
-
         <div>
             <h1>Sign up with Email and Password</h1>
             <form onSubmit={handleSubmit}>
-                <label>Display Name</label>
-                <input 
+                <FormInput
+                    label="Display Name"
                     type="text" 
                     required 
                     onChange={handleChange} 
@@ -69,8 +70,8 @@ const SignUpForm = () => {
                     value={displayName}
                 />
 
-                <label>Email</label>
-                <input 
+                <FormInput
+                    label="Email"
                     type="email" 
                     required 
                     onChange={handleChange} 
@@ -78,8 +79,8 @@ const SignUpForm = () => {
                     value={email}
                 />
 
-                <label>Password</label>
-                <input 
+                <FormInput 
+                    label="Password"
                     type="password" 
                     required 
                     onChange={handleChange} 
@@ -87,8 +88,8 @@ const SignUpForm = () => {
                     value={password}
                 />
 
-                <label>Confirm Password</label>
-                <input 
+                <FormInput 
+                    label="Confirm Password"
                     type="password" 
                     required 
                     onChange={handleChange} 
